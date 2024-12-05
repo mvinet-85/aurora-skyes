10f1306dd1dad2173a8081fc3c344c411c76905e
sharon <sharonmatiedjou@gmail>
2024-12-05 16:53:07 +0100

    login et inscription (debut)

---
+++ aurora-skyes-front/src/app/register/register.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule,],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  phoneNumber: string = '';
+  username: string = '';
+  private auth: any;
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  register() {
+    if (this.username === '') {
+      alert('Please enter a username');
+      return;
+    }
+    if (this.email === '') {
+      alert('Please enter an email');
+      return;
+    }
+    if (this.phoneNumber === '') {
+      alert('Please enter a phone number');
+      return;
+    }
+    if (this.password === '') {
+      alert('Please enter a password');
+      return;
+    }
+    if (this.confirmPassword === '') {
+      alert('Please confirm your password');
+      return;
+    }
+
+    if (this.password !== this.confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    this.auth.register(
+      this.username,
+      this.email,
+      this.password,
+      this.phoneNumber
+    );
+
+    this.email = '';
+    this.password = '';
+    this.confirmPassword = '';
+    this.phoneNumber = '';
+    this.username = '';
+  }
+}
