8e85c3737bc7606d8ec51866737c0aef06c40948
mvinet <vinetmanon85@gmail.com>
2024-12-06 16:00:15 +0100

    Ajout toast

--- aurora-skyes-front/src/app/app.component.ts
+++ aurora-skyes-front/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {ToastComponent} from './shared/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
