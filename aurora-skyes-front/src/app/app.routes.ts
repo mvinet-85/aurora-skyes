fa079a71938275f94eceb985b30a88e6d2bb1127
sharon <sharonmatiedjou@gmail>
2024-12-06 10:32:30 +0100

    modifications et creation de guards

--- aurora-skyes-front/src/app/app.routes.ts
+++ aurora-skyes-front/src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import {Routes} from '@angular/router';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {VerifyEmailComponent} from './verify-email/verify-email.component';
-import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
-
+import {LoginComponent} from './page/login/login.component';
+import {RegisterComponent} from './page/register/register.component';
+import {VerifyEmailComponent} from './page/verify-email/verify-email.component';
+import {ForgotPasswordComponent} from './page/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'register', component: RegisterComponent},
   {path: 'verify-email', component: VerifyEmailComponent},
   {path: 'forgot-password', component: ForgotPasswordComponent},
-
+  {path: '**', redirectTo: 'login'} // Fallback route
 ];
