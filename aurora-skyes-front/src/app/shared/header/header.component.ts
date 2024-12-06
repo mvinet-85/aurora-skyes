23d18e84086f226b5893181790846f8914d59c45
mvinet <vinetmanon85@gmail.com>
2024-12-06 16:00:51 +0100

    Ajout d'un bouton de deconnexion

--- aurora-skyes-front/src/app/shared/header/header.component.ts
+++ aurora-skyes-front/src/app/shared/header/header.component.ts
@@ -1,12 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthService} from "../../core/services/authentification/auth.service";
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  standalone: true,
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    imports: [],
+    templateUrl: './header.component.html',
+    standalone: true,
+    styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+    public authService: AuthService = inject(AuthService);
+
+    deconnexion() {
+        this.authService.logout();
+    }
 }
