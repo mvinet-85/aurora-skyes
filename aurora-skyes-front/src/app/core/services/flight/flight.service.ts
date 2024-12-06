95db7f0a7b26145a6758f226bb6ece99150537ad
mvinet <vinetmanon85@gmail.com>
2024-12-06 14:02:54 +0100

    Ménage

--- aurora-skyes-front/src/app/core/services/flight/flight.service.ts
+++ aurora-skyes-front/src/app/core/services/flight/flight.service.ts
@@ -4,16 +4,16 @@ import {Observable} from 'rxjs';
 import {flight} from '../../models/flight';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FlightService {
 
-  private readonly apiUrl = 'http://localhost:8080/vols';
+    private readonly apiUrl = 'http://localhost:8080/vols';
 
-  constructor(private readonly http: HttpClient) {
-  }
+    constructor(private readonly http: HttpClient) {
+    }
 
-  getAllVols(): Observable<flight[]> {
-    return this.http.get<flight[]>(this.apiUrl);
-  }
+    getAllVols(): Observable<flight[]> {
+        return this.http.get<flight[]>(this.apiUrl);
+    }
 }
