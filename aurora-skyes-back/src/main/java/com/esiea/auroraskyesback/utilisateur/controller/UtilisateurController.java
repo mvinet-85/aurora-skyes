a597f76aae6e9abaf9a40a70ba13cdba3040a73f
mvinet <vinetmanon85@gmail.com>
2024-12-09 08:55:30 +0100

    Envoie d'un responseDTO à la place d'un UtilisateurDTO

--- aurora-skyes-back/src/main/java/com/esiea/auroraskyesback/utilisateur/controller/UtilisateurController.java
+++ aurora-skyes-back/src/main/java/com/esiea/auroraskyesback/utilisateur/controller/UtilisateurController.java
@@ -1,7 +1,12 @@
 package com.esiea.auroraskyesback.utilisateur.controller;
 
+import com.esiea.auroraskyesback.authentification.controller.AuthentificationController;
+import com.esiea.auroraskyesback.authentification.dto.ResponseDTO;
+import com.esiea.auroraskyesback.authentification.utils.JwtUtil;
 import com.esiea.auroraskyesback.utilisateur.dto.UtilisateurDTO;
 import com.esiea.auroraskyesback.utilisateur.service.UtilisateurService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/utilisateurs")
 public class UtilisateurController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthentificationController.class);
 
     /** {@link UtilisateurService} */
     private final UtilisateurService utilisateurService;
 
-    public UtilisateurController(UtilisateurService utilisateurService) {
+    /** {@link JwtUtil} */
+    private final JwtUtil jwtUtil;
+
+    public UtilisateurController(UtilisateurService utilisateurService, JwtUtil jwtUtil) {
         this.utilisateurService = utilisateurService;
+        this.jwtUtil = jwtUtil;
     }
 
     /**
@@ -23,9 +33,16 @@ public class UtilisateurController {
      * @return l'utilisateur créé
      */
     @PostMapping
-    public ResponseEntity<UtilisateurDTO> creerUtilisateur(@RequestBody UtilisateurDTO utilisateurDTO) {
+    public ResponseEntity<ResponseDTO> creerUtilisateur(@RequestBody UtilisateurDTO utilisateurDTO) {
         UtilisateurDTO utilisateur = utilisateurService.creerUtilisateur(utilisateurDTO);
-        return new ResponseEntity<>(utilisateur, HttpStatus.CREATED);
+
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setId(utilisateur.getId());
+        String token = jwtUtil.generateToken(utilisateur.getNom());
+        responseDTO.setToken(token);
+        LOGGER.info("Token JWT généré avec succès pour l'utilisateur : {}", utilisateur.getEmail());
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
 }
