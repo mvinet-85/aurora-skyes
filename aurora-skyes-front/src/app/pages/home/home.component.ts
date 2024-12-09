2437754d0d6e3cc97349e44304c053ce21e1eb70
sharon <sharonmatiedjou@gmail>
2024-12-09 11:23:09 +0100

    modif monnaie

--- aurora-skyes-front/src/app/pages/home/home.component.ts
+++ aurora-skyes-front/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {vol} from '../../core/models/vol';
 import {aeroport} from '../../core/models/aeroport';
-import {DatePipe} from '@angular/common';
+import {DatePipe, NgForOf} from '@angular/common';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {SearchFormModel} from '../../core/models/form';
 import {HeaderComponent} from '../../shared/header/header.component';
@@ -22,7 +22,8 @@ import {MonnaieService} from '../../core/services/monnaie/monnaie.service';
     DatePipe,
     ReactiveFormsModule,
     HeaderComponent,
-    ConfirmModalComponent
+    ConfirmModalComponent,
+    NgForOf
   ],
   templateUrl: './home.component.html',
   standalone: true,
