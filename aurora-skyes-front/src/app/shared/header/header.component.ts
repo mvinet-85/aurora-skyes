9dc80d6585eec0eef20e2a85b301d74344745ff6
mvinet <vinetmanon85@gmail.com>
2024-12-09 15:28:18 +0100

    Ajout select langue

--- aurora-skyes-front/src/app/shared/header/header.component.ts
+++ aurora-skyes-front/src/app/shared/header/header.component.ts
@@ -1,18 +1,21 @@
 import {Component, inject} from '@angular/core';
 import {AuthService} from "../../core/services/authentification/auth.service";
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-header',
-    imports: [],
-    templateUrl: './header.component.html',
-    standalone: true,
-    styleUrl: './header.component.scss'
+  selector: 'app-header',
+  imports: [
+    TranslatePipe
+  ],
+  templateUrl: './header.component.html',
+  standalone: true,
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
-    public authService: AuthService = inject(AuthService);
+  public authService: AuthService = inject(AuthService);
 
-    deconnexion() {
-        this.authService.logout();
-    }
+  deconnexion() {
+    this.authService.logout();
+  }
 }
