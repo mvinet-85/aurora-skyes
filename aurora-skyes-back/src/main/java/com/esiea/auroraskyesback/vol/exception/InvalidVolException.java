2b5e8acc0bb8816b3ff71042b8562232bc497997
mvinet <vinetmanon85@gmail.com>
2024-12-10 09:44:10 +0100

    Ajout des exceptions pour le vol

---
+++ aurora-skyes-back/src/main/java/com/esiea/auroraskyesback/vol/exception/InvalidVolException.java
@@ -0,0 +1,7 @@
+package com.esiea.auroraskyesback.vol.exception;
+
+public class InvalidVolException extends RuntimeException {
+    public InvalidVolException(String message) {
+        super(message);
+    }
+}
---
+++ aurora-skyes-back/src/main/java/com/esiea/auroraskyesback/vol/exception/VolNotFoundException.java
@@ -0,0 +1,7 @@
+package com.esiea.auroraskyesback.vol.exception;
+
+public class VolNotFoundException extends RuntimeException {
+    public VolNotFoundException(String message) {
+        super(message);
+    }
+}
