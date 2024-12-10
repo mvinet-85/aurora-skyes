5837fd8ce7bbaf3d597bde9914753df61157ef75
mvinet <vinetmanon85@gmail.com>
2024-12-10 14:43:52 +0100

    Mise en place de la récupération des données

--- aurora-skyes-front/src/app/core/services/statistique/reservation-stat.service.ts
+++ aurora-skyes-front/src/app/core/services/statistique/reservation-stat.service.ts
@@ -3,43 +3,43 @@ import {Observable} from 'rxjs';
 import {reservationStat} from '../../models/reservation-stat';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ReservationStatService {
 
-  private eventSource: EventSource | null = null;
-  private url: string = 'http://localhost:8080/historique';
+    private eventSource: EventSource | null = null;
+    private url: string = 'http://localhost:8080/reservations/historique';
 
-  constructor() {
-  }
-
-  startListening(): Observable<reservationStat> {
-    return new Observable(observer => {
-      this.eventSource = new EventSource(this.url);
-
-      this.eventSource.onmessage = (event) => {
-        try {
-          const data: reservationStat = JSON.parse(event.data);
-          observer.next(data);
-        } catch (error) {
-          observer.error('Erreur lors du parsing des données SSE');
-        }
-      };
-
-      this.eventSource.onerror = (error) => {
-        observer.error(error);
-        this.closeConnection();
-      };
-
-      return () => {
-        this.closeConnection();
-      };
-    });
-  }
-
-  private closeConnection(): void {
-    if (this.eventSource) {
-      this.eventSource.close();
+    constructor() {
     }
-  }
+
+    startListening(): Observable<reservationStat> {
+        return new Observable(observer => {
+            this.eventSource = new EventSource(this.url);
+
+            this.eventSource.onmessage = (event) => {
+                try {
+                    const data: reservationStat = JSON.parse(event.data);
+                    observer.next(data);
+                } catch (error) {
+                    observer.error('Erreur lors du parsing des données SSE');
+                }
+            };
+
+            this.eventSource.onerror = (error) => {
+                observer.error(error);
+                this.closeConnection();
+            };
+
+            return () => {
+                this.closeConnection();
+            };
+        });
+    }
+
+    private closeConnection(): void {
+        if (this.eventSource) {
+            this.eventSource.close();
+        }
+    }
 }
--- aurora-skyes-front/src/app/pages/reservation-state/reservation-state.component.ts
+++ aurora-skyes-front/src/app/pages/reservation-state/reservation-state.component.ts
@@ -1,40 +1,40 @@
 import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {HeaderComponent} from '../../shared/header/header.component';
-import {ReservationStatService} from '../../core/services/statistique/reservation-stat.service';
-import {reservationStat} from '../../core/models/reservation-stat';
-import {Subscription} from 'rxjs';
-import {DatePipe} from '@angular/common';
+import {reservationStat} from "../../core/models/reservation-stat";
+import {ReservationStatService} from "../../core/services/statistique/reservation-stat.service";
+import {Subscription} from "rxjs";
+import {DatePipe} from "@angular/common";
 
 @Component({
-  selector: 'app-reservation-state',
-  imports: [
-    HeaderComponent,
-    DatePipe
-  ],
-  templateUrl: './reservation-state.component.html',
-  standalone: true,
-  styleUrl: './reservation-state.component.scss'
+    selector: 'app-reservation-state',
+    imports: [
+        HeaderComponent,
+        DatePipe
+    ],
+    templateUrl: './reservation-state.component.html',
+    standalone: true,
+    styleUrl: './reservation-state.component.scss'
 })
 export class ReservationStateComponent implements OnInit, OnDestroy {
 
-  public reservationStats: reservationStat | null = null;
-  private reservationStatService: ReservationStatService = inject(ReservationStatService);
-  private subscription: Subscription = inject(Subscription);
+    public reservationStats: reservationStat | null = null;
+    private reservationStatService: ReservationStatService = inject(ReservationStatService);
+    private subscription: Subscription | null = null;
 
-  ngOnInit(): void {
-    this.subscription = this.reservationStatService.startListening().subscribe(
-      (data: reservationStat) => {
-        this.reservationStats = data;
-      },
-      (error) => {
-        console.error('Erreur SSE:', error);
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
+    ngOnInit(): void {
+        this.subscription = this.reservationStatService.startListening().subscribe(
+            (data: reservationStat) => {
+                this.reservationStats = data;
+            },
+            (error) => {
+                console.error('Erreur SSE:', error);
+            }
+        );
     }
-  }
+
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
 }
