1d643c0a77451b6c38b14a38c8a01a252779aeac
mvinet <vinetmanon85@gmail.com>
2024-12-10 11:16:35 +0100

    Ajout de la gestion sse

---
+++ aurora-skyes-front/src/app/core/models/reservation-stat.ts
@@ -0,0 +1,5 @@
+export interface reservationStat {
+  reservationOK: number;
+  reservationKO: number;
+  date: Date;
+}
--- aurora-skyes-front/src/app/core/services/statistique/reservation-stat.service.ts
+++ aurora-skyes-front/src/app/core/services/statistique/reservation-stat.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {reservationStat} from '../../models/reservation-stat';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationStatService {
 
-  constructor() { }
+  private eventSource: EventSource | null = null;
+  private url: string = 'http://localhost:8080/historique';
+
+  constructor() {
+  }
+
+  startListening(): Observable<reservationStat> {
+    return new Observable(observer => {
+      this.eventSource = new EventSource(this.url);
+
+      this.eventSource.onmessage = (event) => {
+        try {
+          const data: reservationStat = JSON.parse(event.data);
+          observer.next(data);
+        } catch (error) {
+          observer.error('Erreur lors du parsing des données SSE');
+        }
+      };
+
+      this.eventSource.onerror = (error) => {
+        observer.error(error);
+        this.closeConnection();
+      };
+
+      return () => {
+        this.closeConnection();
+      };
+    });
+  }
+
+  private closeConnection(): void {
+    if (this.eventSource) {
+      this.eventSource.close();
+    }
+  }
 }
