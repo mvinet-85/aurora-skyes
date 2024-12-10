89c2c623f7732b06004fdda603f647288c7c70fb
mvinet <vinetmanon85@gmail.com>
2024-12-10 10:51:09 +0100

    Ajout de la route

--- aurora-skyes-front/src/app/app.routes.ts
+++ aurora-skyes-front/src/app/app.routes.ts
@@ -5,7 +5,12 @@ import {VerifyEmailComponent} from './pages/authentification/verify-email/verify
 import {ForgotPasswordComponent} from './pages/authentification/forgot-password/forgot-password.component';
 import {HomeComponent} from './pages/home/home.component';
 import {AuthGuard} from './core/guards/auth.guard';
+<<<<<<< HEAD
 import {ReservationComponent} from './pages/reservation/reservation.component';
+import { ReservationStateComponent } from './pages/reservation-state/reservation-state.component';
+=======
+import {ReservationStateComponent} from './pages/reservation-state/reservation-state.component';
+>>>>>>> ff0c03c (Ajout de la route)
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -18,6 +23,9 @@ export const routes: Routes = [
   },
   {
     path: 'reservation', component: ReservationComponent, canActivate: [AuthGuard],
+  },
+  {
+    path: 'stat', component: ReservationStateComponent, canActivate: [AuthGuard],
   },
   {path: '**', redirectTo: 'login'}
 ];
