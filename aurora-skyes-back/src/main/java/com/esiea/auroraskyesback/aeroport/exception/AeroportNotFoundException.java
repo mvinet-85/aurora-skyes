ff6351cd8e7bbf62cf24fe1e09c40c1e12931f55
mvinet <vinetmanon85@gmail.com>
2024-12-10 10:01:15 +0100

    Suppression exceptions aeroport

--- aurora-skyes-back/src/main/java/com/esiea/auroraskyesback/aeroport/exception/AeroportNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.esiea.auroraskyesback.aeroport.exception;
-
-public class AeroportNotFoundException extends RuntimeException {
-  public AeroportNotFoundException(String message) {
-    super(message);
-  }
-}
--- aurora-skyes-back/src/main/java/com/esiea/auroraskyesback/aeroport/exception/InvalidAeroportException.java
+++
@@ -1,7 +0,0 @@
-package com.esiea.auroraskyesback.aeroport.exception;
-
-public class InvalidAeroportException extends RuntimeException {
-    public InvalidAeroportException(String message) {
-        super(message);
-    }
-}
