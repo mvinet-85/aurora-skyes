87cf1d88439cef9fc25569391cfd6b7b87a11509
mvinet <vinetmanon85@gmail.com>
2024-12-11 08:48:16 +0100

    Données aléatoire en attendant

--- aurora-skyes-front/src/app/core/services/statistique/reservation-stat.service.ts
+++ aurora-skyes-front/src/app/core/services/statistique/reservation-stat.service.ts
@@ -1,47 +1,57 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {interval, map, Observable} from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ReservationStatService {
+  private readonly eventSourceReservationOK: EventSource | null = null;
+  private readonly eventSourceReservationError: EventSource | null = null;
+  private readonly urlReservationOK: string = 'http://localhost:8080/reservations/historique';
+  private readonly urlReservationError: string = 'http://localhost:8080/reservations/historique/error';
 
-    private eventSource: EventSource | null = null;
-    private url: string = 'http://localhost:8080/reservations/historique';
+  constructor() {
+  }
 
-    constructor() {
+  startListening(volId: number): Observable<{ event: string, data: any }> {
+    // return new Observable((observer: Observer<{ event: string, data: any }>) => {
+    //   this.eventSourceReservationOK = new EventSource(`${this.urlReservationOK}/${volId}`);
+    //
+    //   this.eventSourceReservationOK.onmessage = (event) => {
+    //     observer.next({event: 'ReservationOK', data: event.data});
+    //   };
+    //
+    //   this.eventSourceReservationError = new EventSource(`${this.urlReservationError}/${volId}`);
+    //   this.eventSourceReservationError.onmessage = (event) => {
+    //     observer.next({event: 'ReservationError', data: event.data});
+    //   };
+    //
+    //   this.eventSourceReservationOK.onerror = (error) => {
+    //     observer.error(error);
+    //     this.closeConnection();
+    //   };
+    //   this.eventSourceReservationError.onerror = (error) => {
+    //     observer.error(error);
+    //     this.closeConnection();
+    //   };
+    // });
+    return interval(6000).pipe(
+      map(() => {
+        const isOK = Math.random() > 0.5;
+        return {
+          event: isOK ? 'ReservationOK' : 'ReservationError',
+          data: isOK ? Math.floor(Math.random() * 100) + 1 : 1
+        };
+      })
+    );
+  }
+
+  private closeConnection(): void {
+    if (this.eventSourceReservationOK) {
+      this.eventSourceReservationOK.close();
     }
-
-    startListening(volId: number): Observable<{ event: string, data: any }> {
-        return new Observable(observer => {
-            this.eventSource = new EventSource(`${this.url}/${volId}`);
-
-            this.eventSource.onmessage = (event) => {
-                observer.next({event: 'message', data: event.data});
-            };
-
-            this.eventSource.addEventListener('ReservationOK', (event: MessageEvent) => {
-                observer.next({event: 'ReservationOK', data: JSON.parse(event.data)});
-            });
-
-            this.eventSource.addEventListener('ReservationError', (event: MessageEvent) => {
-                observer.next({event: 'ReservationError', data: event.data});
-            });
-
-            this.eventSource.onerror = (error) => {
-                observer.error(error);
-                this.closeConnection();
-            };
-
-            return () => {
-                this.closeConnection();
-            };
-        });
+    if (this.eventSourceReservationError) {
+      this.eventSourceReservationError.close();
     }
-
-    private closeConnection(): void {
-        if (this.eventSource) {
-            this.eventSource.close();
-        }
-    }
+  }
 }
